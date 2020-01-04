1dfe3ce10aa8c5eb6a22629b3b70a3caf19e276b
alexandregjr <alexandregalochajr@gmail.com>
2020-01-04 14:02:25 -0300

    jump off & better movement

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -61,49 +61,49 @@ public class CharacterController2D : MonoBehaviour
 	}
 
 
-	public void Move(float move, bool crouch, bool jump)
+	public void Move(float move, bool jump)
 	{
 		// If crouching, check to see if the character can stand up
-		if (!crouch)
-		{
-			// If the character has a ceiling preventing them from standing up, keep them crouching
-			//if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
-			//{
-			//	crouch = true;
-			//}
-		}
+		// if (!crouch)
+		// {
+		// 	// If the character has a ceiling preventing them from standing up, keep them crouching
+		// 	//if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
+		// 	//{
+		// 	//	crouch = true;
+		// 	//}
+		// }
 
 		//only control the player if grounded or airControl is turned on
 		if (m_Grounded || m_AirControl)
 		{
 
 			// If crouching
-			if (crouch)
-			{
-				if (!m_wasCrouching)
-				{
-					m_wasCrouching = true;
-					OnCrouchEvent.Invoke(true);
-				}
+			// if (crouch)
+			// {
+			// 	if (!m_wasCrouching)
+			// 	{
+			// 		m_wasCrouching = true;
+			// 		OnCrouchEvent.Invoke(true);
+			// 	}
 
-				// Reduce the speed by the crouchSpeed multiplier
-				move *= m_CrouchSpeed;
+			// 	// Reduce the speed by the crouchSpeed multiplier
+			// 	move *= m_CrouchSpeed;
 
-				// Disable one of the colliders when crouching
-				if (m_CrouchDisableCollider != null)
-					m_CrouchDisableCollider.enabled = false;
-			} else
-			{
-				// Enable the collider when not crouching
-				if (m_CrouchDisableCollider != null)
-					m_CrouchDisableCollider.enabled = true;
+			// 	// Disable one of the colliders when crouching
+			// 	if (m_CrouchDisableCollider != null)
+			// 		m_CrouchDisableCollider.enabled = false;
+			// } else
+			// {
+			// 	// Enable the collider when not crouching
+			// 	if (m_CrouchDisableCollider != null)
+			// 		m_CrouchDisableCollider.enabled = true;
 
-				if (m_wasCrouching)
-				{
-					m_wasCrouching = false;
-					OnCrouchEvent.Invoke(false);
-				}
-			}
+			// 	if (m_wasCrouching)
+			// 	{
+			// 		m_wasCrouching = false;
+			// 		OnCrouchEvent.Invoke(false);
+			// 	}
+			// }
 
 			// Move the character by finding the target velocity
 			Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,32 +5,63 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public CharacterController2D controller;
-    
+    // public CharacterController2D controller;
+    public PlatformEffector2D platforms;
+
     float runSpeed = 40f;
+    float jumpForce = 700f;
+    float movementSmoothing = .05f;
     float hMove = 0f; 
     float yMove = 0f; 
     bool jumping = false;
-    bool crouching = false;
+    bool runningJumpOff = false;
+    int playerLayer, platformLayer;
+    Rigidbody2D rb;
+    private Vector3 velocity = Vector3.zero;
 
-    public void OnMove(InputValue input)
-    {
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        playerLayer = LayerMask.NameToLayer ("Player");
+		platformLayer = LayerMask.NameToLayer ("Platform");
+        Debug.Log(playerLayer);
+        Debug.Log(platformLayer);
+    }
+
+    public void OnMove(InputValue input) {
         hMove = input.Get<float>();
     }
 
-    public void OnJump()
-    {
+    public void OnJump() {
         jumping = true;
     }
 
-    public void OnCrouch()
-    {
-        crouching = true;
+    public void OnCrouch() {
+        // TODO: Check if is in
+        if (!runningJumpOff && rb.velocity.y == 0) {
+            StartCoroutine ("JumpOff");
+        }
     }
 
     void FixedUpdate() {
-        controller.Move(hMove * runSpeed * Time.fixedDeltaTime, false, jumping);
+        float move = hMove * runSpeed * Time.fixedDeltaTime;
+        Vector3 targetVelocity = new Vector2(move * 10f, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, movementSmoothing);
+
+        if (jumping && rb.velocity.y == 0) {
+            rb.AddForce(new Vector2(0f, jumpForce));
+        }
+
         jumping = false;
-        crouching = false;
     }
+
+	IEnumerator JumpOff()
+	{
+		runningJumpOff = true;
+		platforms.rotationalOffset = 180;
+        platforms.surfaceArc = 0;
+        yield return new WaitForSeconds (0.4f);
+		platforms.rotationalOffset = 0;
+        platforms.surfaceArc = 180;
+		runningJumpOff = false;
+	}
 }
